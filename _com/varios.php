9dc6fa4a96b0cb3eefaeb54b67acf60ff93fbdf6
Anas Benali <anas.benali@educa.madrid.org>
2020-12-30 19:46:25 +0100

    continuacion de las clases.

---
+++ _com/varios.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+function redireccionar(string $url)
+{
+    header("Location: $url");
+    exit;
+}
+
+function syso(string $contenido)
+{
+    file_put_contents('php://stderr', $contenido . "\n");
+}
+
+function obtenerFecha(): string
+{
+    return date("Y-m-d H:i:s");
+}
+
+function generarCadenaAleatoria($longitud) : string
+{
+    for ($s = '', $i = 0, $z = strlen($a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')-1; $i != $longitud; $x = rand(0,$z), $s .= $a[$x], $i++);
+    return $s;
+}
